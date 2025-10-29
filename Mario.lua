a28c9e4f756e430609572faa4bee4a2ce138a032
Xander C <carroll.892@osu.edu>
2025-10-29 09:56:03 -0400

    GetPlayerPosition Function.

--- Mario.lua
+++ Mario.lua
@@ -1,8 +1,10 @@
--- Import RAM addresses
-local ADDRESS_TILES = 0x0500;   -- The tile array/matrix
-local ADDRESS_SCREEN = 0x006D;  -- The current screen within the level
-local ADDRESS_HPOS = 0x0086;    -- The players horizontal position (in the current screen)
-local ADDRESS_VPOS = 0x03B8;    -- The players vertical position
+---- CONSTANT VARIABLES
+
+-- Important RAM addresses
+local ADDRESS_TILES = 0x0500;   -- Array<int>*   : The tile array/matrix
+local ADDRESS_SCREEN = 0x006D;  -- int*          : The current screen within the level
+local ADDRESS_HPOS = 0x0086;    -- int*          : The players horizontal position (in the current screen)
+local ADDRESS_VPOS = 0x03B8;    -- int*          : The players vertical position
 
 -- How wide and tall a RAM screen section is in tiles
 local SCREEN_HEIGHT = 13;
@@ -11,56 +13,51 @@ local SCREEN_WIDTH = 32;
 -- The size of each tile in pixels
 local TILE_WIDTH = 8;
 
--- Keys that are currently being pressed
-local k1;
 
--- Checks what keys are currently being pressed
-function checkInputs()
-    local inputs = input.get()
-    k1 = inputs["Insert"]
-end
 
--- Draw the player's global world position to the screen. 
-function drawPlayerPosition()
+---- SCRIPT VARIABLES
+
+-- table<string, boolean> : Keys that are currently being pressed
+local inputs;
+
+-- int, int : The player's global world position
+local playerX, playerY;
+
+
+
+---- UTILITY FUNCTIONS
+
+-- Get the player's position
+function getPlayerPosition()
     -- Get the player's x and y position in the world.
-    local xPos = memory.read_u8(ADDRESS_HPOS) + (SCREEN_WIDTH*TILE_WIDTH)*memory.read_u8(ADDRESS_SCREEN);
-    local yPos = memory.read_u8(ADDRESS_VPOS);
 
-    -- Draw the player's position.
-	local text = "Position: (" .. xPos .. ", " .. yPos .. ")";
-	gui.drawText(10,30,text,0xFFFFFFFF,16);
+    -- Calculated as (screen position + screen_width*number_of_screens_cleared)
+    playerX = memory.readbyte(ADDRESS_HPOS) + (SCREEN_WIDTH*TILE_WIDTH)*memory.readbyte(ADDRESS_SCREEN);
+    
+    -- Calcualted as (screen position + mario_height)
+    playerY = memory.readbyte(ADDRESS_VPOS) + 2*TILE_WIDTH;
 end
 
--- Will print all of the RAM associated with tiles. (Prints the world). 
-function printScreen()
-    -- Clear the console.
-    console.clear()
 
-    -- Get all of the RAM associated with tiles.
-    local level = memory.read_bytes_as_array(ADDRESS_TILES, SCREEN_WIDTH*SCREEN_HEIGHT);
 
-    -- Prints the array in a human readable format.
-    for y=1,SCREEN_HEIGHT do
-        local line = ""
-        for x=1,SCREEN_WIDTH/2 do
-            if(x == playerH) then line = line .. " ***" else
-                line = line .. " " .. string.format("%3x", level[(y-1)*(SCREEN_WIDTH/2) + x])
-            end
-        end
-        for x=1,SCREEN_WIDTH/2 do
-            line = line .. " " .. string.format("%3x", level[(y-1+SCREEN_HEIGHT)*(SCREEN_WIDTH/2) + x])
-        end
-        console.log(line)
-    end
-end
+---- MAIN GAME LOOP
 
 while true do
-    -- Done every frame
-	emu.frameadvance();
-    checkInputs();
+    -- Get the inputs every frame
+    inputs = input.get()
 
     -- Draw/Print useful data
-	drawPlayerPosition();
-    if(k1) then printScreen() end
-    
+    getPlayerPosition();
+
+
+    -- TODO: Remove Debug Statement
+    if(inputs["Insert"]) then 
+        print("[DEBUG]:");
+    end
+
+    -- TODO: Remove. Draw the player's position.
+	gui.drawText(10,30,"Position: (" .. playerX .. ", " .. playerY .. ")",0xFFFFFFFF,16);
+
+    -- Advance the frame
+    emu.frameadvance();
 end
